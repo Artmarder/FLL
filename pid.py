73fbba0600d5df12fd6430f949c261143ac6b0ce
Artem Marderfeld <160776853+Artmarder@users.noreply.github.com>
2024-07-30 11:24:50 +0300

    Update pid.py

--- pid.py
+++ pid.py
@@ -1,45 +1,65 @@
 import micropython as micro
-from time import time as time
-from MyDevice import all
-from myTools.py import all
-from odomerty import all
+from time import time
+from MyDevice import *
+from myTools import *
+from odometry import *
 
-class PIDController():
-    def __init__(self, kp, ki, kd, error, feedback, target) -> float:
+class PIDController:
+    def init(self, kp, ki, kd, target):
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.error = error
-        self.feedback = feedback
-        self.lastError = lastError = 0
+        self.integral = 0
+        self.lastError = 0
         self.lastTime = time()
         self.target = target
     
-    
     @micro.native
-    def correction():
-
-        error = target - GetAngel
-
-        
-        deltaTime = time - lastTime
+    def correction(self, current_angle):
+        current_time = time()
+        error = self.target - current_angle
+        deltaTime = current_time - self.lastTime
+        if deltaTime == 0:
+            deltaTime += 0.001
         
         proportional = error
-        integral += (error + lastError) / 2 * deltaTime
-        differential = (error - lastError) / deltaTime
+        self.integral += (error + self.lastError) / 2 * deltaTime
+        differential = (error - self.lastError) / deltaTime if deltaTime != 0 else 0
+        
+        output.correction = self.kp * proportional + self.ki * self.integral + self.kd * differential
+        
+        self.lastError = error
+        self.lastTime = current_time
+        
+        return pid_a
 
-        return kp * proportional + ki * integral + kd * differential
+    def drive(self, speed, wantedDistance):
+        current_distance = calculate_distance()
 
-
-    def drive(speed, wantedDistance) -> float:
-        correction = correction()
-        Distance = calculate_distance()
-
-        while Distance > wantedDistance:
-            if speed > 0:
-                error = target - GetAngel
-                if error < -180:
+        while current_distance < wantedDistance:
+            current_angle = GetAngle()
+            correction = self.correction(current_angle)
+            error = self.target - current_angle
+            if error < -180:
                 error += 360
-                run_tank(speed+correction, speed - correction)
+            run_tank(speed + correction, speed - correction)
+            current_distance = calculate_distance()
 
-        motor.stop
+        motor.stop()
+
+    def turn(self, target, speed):
+        pid = PIDController(self.kp, self.ki, self.kd, target)
+
+        while True:
+            current_angle = GetAngle()
+            error = target - current_angle
+            if abs(error) < 1:
+                break
+
+            if error < -180:
+                error += 360
+
+            correction = pid.correction(current_angle)
+            run_tank(correction, -correction)
+        
+        motor.stop()
