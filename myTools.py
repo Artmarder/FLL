b687b63480ada180e7912d6222fe0a3c58f5fc67
Artem Marderfeld <160776853+Artmarder@users.noreply.github.com>
2024-10-04 14:04:15 +0300

    delete the thing that i dont need

--- myTools.py
+++ myTools.py
@@ -1,33 +1,15 @@
-import micropython
 from _thread import start_new_thread
-from time import time
 
-def mean():
-    print('hello world !')
 
-class Timer:
-    def __init__(self):
-        self.start_time = time()
-        self.pause_time = None
+def thread(func):
+    """
+    This decorator return the given function as a thread.
 
-    def get_elapsed_time(self):
-        """Returns elapsed time in seconds."""
-        if self.pause_time is None:
-            return time() - self.start_time
-        else:
-            return self.pause_time - self.start_time
-
-    def reset(self):
-        self.start_time = time()
-        self.pause_time = None
-
-    def pause(self):
-        self.pause_time = time()
-
-    def resume(self):
-        if self.pause_time is not None:
-            elapsed = time() - self.pause_time
-            self.start_time += elapsed
-            self.pause_time = None
+    Parameters: func: Function
+    Returns: func: Thread
+    """
+    def wrapper(*args, **kwargs):
+        start_new_thread(func, args, kwargs)
+    return wrapper
 
 
